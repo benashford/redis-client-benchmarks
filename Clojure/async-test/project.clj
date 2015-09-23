d283dfd1ae5b68cdb2e3191132b61f7aa2bc80ee
Ben Ashford <benashford@users.noreply.github.com>
2015-09-23 18:12:32 +0100

    Use redis-async 0.3.2

--- Clojure/async-test/project.clj
+++ Clojure/async-test/project.clj
@@ -1,5 +1,5 @@
 (defproject async-test "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [redis-async "0.3.1"]]
+                 [redis-async "0.3.2"]]
   :main async-test.core
   :aot [async-test.core])
